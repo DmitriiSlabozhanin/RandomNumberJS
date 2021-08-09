a2424aa13eb19581895b5fd964ad7f6c3c6c30da
DmitriiSlabozhanin <senya@vtomske.ru>
2021-08-09 23:48:03 +0700

    add lesson06

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 'use strict';
 
+let randNumber = Math.floor(Math.random() * 101);
+    console.log(randNumber);
+
+function isNumber(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+  }
+
+function yourNumber() {
+  let yourNum = prompt("Угадай число от 1 до 100");
+  if (yourNum === null) {
+    alert("Игра окончена");
+  } else if (isNumber(yourNum)) {
+    yourNum = +yourNum;
+
+    if (yourNum > randNumber) {
+        alert ("Загаданное число меньше");
+    } else if (yourNum < randNumber) {
+        alert ("Загаданное число больше");
+    } else if (yourNum === randNumber) {
+        alert ("Поздравляю, Вы угадали!!!");
+        return yourNum;
+    }
+  } else {
+    confirm ("Введи число!");
+  }
+  return yourNumber;
+}
+
+console.log(yourNumber());
